8c08ebd3accac770aba3cfd674a75a2e32ea3e19
pnrbrk41 <113897255+pnrbrk41@users.noreply.github.com>
2022-10-04 00:00:39 +0300

    s
    s

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a= 2;
+            int b= 3;
+            Console.WriteLine(a+b);
+
+            int sonuc= topla(a,b);
+            Console.WriteLine(sonuc);
+
+            Metotlar ornek= new Metotlar();
+            ornek.ekranaYazdir(Convert.ToString(sonuc));
+
+            int sonuc2= ornek.ArttirveTopla(a,b);
+            ornek.ekranaYazdir(Convert.ToString(sonuc2));
+            ornek.ekranaYazdir(Convert.ToString(a+b));
+
+
+        }
+
+        static int topla(int deger1, int deger2)
+        {
+            return (deger1+deger2);
+        }
+
+
+    }
+
+    class Metotlar{
+        public void ekranaYazdir(string veri){
+            Console.WriteLine(veri);
+
+        }
+        public int ArttirveTopla(int deger1, int deger2){
+            deger1+=1;
+            deger2+=1;
+            return deger1+ deger2;
+
+        }
+    }
+}
